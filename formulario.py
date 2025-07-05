1ede6b31741029b2b00a016a9def030cd3c6cb9a
Sebastian1524 <sebas15caceres@gmail.com>
2025-07-04 21:00:09 -0500

    v2

--- formulario.py
+++ formulario.py
@@ -1,8 +1,40 @@
+from lista_datos import Lista_numeros
+
 class Calculadora:
+    
+    def __init__(self):
+        self.objlista = Lista_numeros()
+        self.numero1 = ""
+        self.numero2 = ""
+        
     def pedir_numero(self):
-        numero1 = input("Ingrese 1:")
-        numero2 = input("Ingrese 2:")
+        numero1 = int(input("Ingrese 1:"))
+        numero2 = int(input("Ingrese 2:"))
         return numero1, numero2
+    
+    
     def almacenar_numero(self):
-        dato_numero = [self.numero1, self.numero2]
-        self.objlista.guardar_numero(dato_numero)
+        self.numero1, self.numero2 = self.pedir_numero()
+        data_numero = [self.numero1, self.numero2]
+        self.objlista.guardar_numero(data_numero)
+
+        
+    def insertar_valor(self):
+        pos = int(input("Posición donde insertar: "))
+        val1 = int(input("Valor 1 a insertar: "))
+        val2 = int(input("Valor 2 a insertar: "))
+        self.objlista.insertar_dato(pos, val1, val2)
+        self.ver_numero()
+    
+    def eliminar_valor(self):
+        self.objlista.eliminar_dato()
+        self.ver_numero()
+        
+    def ver_numero(self):
+        self.objlista.ver_numero()
+
+
+if __name__ == "__main__":
+    calc = Calculadora()
+    calc.almacenar_numero()
+    calc.insertar_valor()
--- lista_datos.py
+++ lista_datos.py
@@ -1,21 +1,22 @@
+
 class Lista_numeros:
     def __init__(self):
         self.lista_numero=[]
         
     def guardar_numero(self,dato_numero):
         self.lista_numero.append(dato_numero)
-    def incluir_listas(self):
-        pass
-    def insertar_dato(self):
-        pass
+        print(self.lista_numero)
+        
+    def incluir_listas(self, otra_lista: list):
+        self.lista_numero.extend(otra_lista)
+        
+    def insertar_dato(self, posicion: int, valor1: int, valor2: int):
+        self.lista_numero.insert(posicion[valor1, valor2])
+        
     def eliminar_dato(self):
-        pass
-    
-    
-    
-    
-    
-    
-    
+        self.lista_numero.pop(int(input("Ingrese la posición a eliminar: ")))
+        
+        
+        
     def ver_numero(self):
-        pass
+        print("Contenido de la lista:", self.lista_numero)
